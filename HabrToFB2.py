13968e8d3e3cbcb2a6b56940433e282bc1c763a1
Apollon76 <Apollon76@yandex.ru>
2014-08-11 10:08:03 +0600

    Update HabrToFB2.py
    Changed names of the files. Added some items to document description. Supported subtitles.

--- HabrToFB2.py
+++ HabrToFB2.py
@@ -18,14 +18,21 @@ def get_page_title(s):
 
 page_address = input('Введите URL:')
 s = urlopen(page_address).read().decode()
+#s = open('input.txt').read()
+file_name = list(filter(lambda s: len(s) > 0, page_address.split('/')))[-1]
 page_title = get_page_title(s)
 page_content = get_page_content(s)
 images = re.findall(r'<img.*?>', page_content)
 images_names = list(map(lambda s: re.findall(r'src=".*?"', s)[0][len('src="') : -1], images))
 images = []
+for num, i in enumerate(images_names):
+	if i[:5] != 'http:':
+		images_names[num] = 'http:' + i
 for i in images_names:
 	images += [str(base64.b64encode(urlopen(i).read()))[2 : -1]]
 page_content = re.sub(r'<img src=".*?>', '<image l:href="#img' + chr(0) + '.jpg"/>', page_content)
+page_content = re.sub(r'<h[0-9]*?>', '<subtitle>', page_content)
+page_content = re.sub(r'</h[0-9]*?>', '</subtitle>', page_content)
 s1 = ''
 q = 0
 for i in page_content:
@@ -36,15 +43,19 @@ for i in page_content:
 		s1 += i
 page_content = s1
 page_content = page_content.split('\n')
-for num, i in enumerate(page_content):
-	page_content[num] = '<p>' + i + '</p>'
-sys.stdout = open(page_title + '.fb2', 'w')
+page_content = list(map(lambda s: '<p>' + s + '</p>', page_content))
+sys.stdout = open(file_name + '.fb2', 'w')
 elements = ['<?xml version="1.0" encoding="utf-8"?>',
 	'<FictionBook xmlns="http://www.gribuser.ru/xml/fictionbook/2.0" xmlns:l="http://www.w3.org/1999/xlink">',
 	'	<description>',
 	'		<title-info>',
+	'			<genre>article</genre>',
 	'			<book-title>' + page_title + '</book-title>',
+	'			<lang>ru</lang>',
 	'		</title-info>',
+	'		<document-info>',
+	'			<src-url>' + page_address + '</src-url>',
+	'		</document-info>',
 	'	</description>',
 	'<body>',
 	'<title>',
