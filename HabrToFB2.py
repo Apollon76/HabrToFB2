ca76879a952ee77d3ed27e3ea294c907df8b7f5b
Apollon76 <Apollon76@yandex.ru>
2014-08-11 11:08:01 +0600

    Update HabrToFB2.py

--- HabrToFB2.py
+++ HabrToFB2.py
@@ -9,6 +9,8 @@ def get_page_content(s):
 	page_content = re.sub(r'<br>', '\n', page_content)
 	page_content = re.sub(r'<br/>', '\n', page_content)
 	page_content = re.sub(r'<iframe.*?</iframe>', 'Здесь было видео!', page_content)
+	page_content = re.sub(r'<b>', '<strong>', page_content)
+	page_content = re.sub(r'</b>', '</strong>', page_content)
 	return page_content
 
 def get_page_title(s):
@@ -16,18 +18,103 @@ def get_page_title(s):
 	page_title = page_title[len('<span class="post_title">'): -len('</span>')]
 	return page_title
 
+def transliterate(string):
+	capital_letters = {u'А': u'A',
+					   u'Б': u'B',
+					   u'В': u'V',
+					   u'Г': u'G',
+					   u'Д': u'D',
+					   u'Е': u'E',
+					   u'Ё': u'E',
+					   u'Ж': u'Zh',
+					   u'З': u'Z',
+					   u'И': u'I',
+					   u'Й': u'Y',
+					   u'К': u'K',
+					   u'Л': u'L',
+					   u'М': u'M',
+					   u'Н': u'N',
+					   u'О': u'O',
+					   u'П': u'P',
+					   u'Р': u'R',
+					   u'С': u'S',
+					   u'Т': u'T',
+					   u'У': u'U',
+					   u'Ф': u'F',
+					   u'Х': u'H',
+					   u'Ц': u'Ts',
+					   u'Ч': u'Ch',
+					   u'Ш': u'Sh',
+					   u'Щ': u'Sch',
+					   u'Ъ': u'',
+					   u'Ы': u'Y',
+					   u'Ь': u'',
+					   u'Э': u'E',
+					   u'Ю': u'Yu',
+					   u'Я': u'Ya'}
+	lower_case_letters = {u'а': u'a',
+					   u'б': u'b',
+					   u'в': u'v',
+					   u'г': u'g',
+					   u'д': u'd',
+					   u'е': u'e',
+					   u'ё': u'e',
+					   u'ж': u'zh',
+					   u'з': u'z',
+					   u'и': u'i',
+					   u'й': u'y',
+					   u'к': u'k',
+					   u'л': u'l',
+					   u'м': u'm',
+					   u'н': u'n',
+					   u'о': u'o',
+					   u'п': u'p',
+					   u'р': u'r',
+					   u'с': u's',
+					   u'т': u't',
+					   u'у': u'u',
+					   u'ф': u'f',
+					   u'х': u'h',
+					   u'ц': u'ts',
+					   u'ч': u'ch',
+					   u'ш': u'sh',
+					   u'щ': u'sch',
+					   u'ъ': u'',
+					   u'ы': u'y',
+					   u'ь': u'',
+					   u'э': u'e',
+					   u'ю': u'yu',
+					   u'я': u'ya'}
+	translit_string = ""
+	for index, char in enumerate(string):
+		if char in lower_case_letters.keys():
+			char = lower_case_letters[char]
+		elif char in capital_letters.keys():
+			char = capital_letters[char]
+			if len(string) > index+1:
+				if string[index+1] not in lower_case_letters.keys():
+					char = char.upper()
+			else:
+				char = char.upper()
+		translit_string += char
+	return translit_string
+
+def get_file_name(s):
+	letters = 'qwertyuiopasdfghjklzxcvbnmйцукенгшщзхъфывапролджэячсмитьбю'
+	s = ''.join(list(map(lambda x: x if x.lower() in letters else ' ', s)))
+	s = transliterate(s)
+	return s
+
 page_address = input('Введите URL:')
 s = urlopen(page_address).read().decode()
 #s = open('input.txt').read()
-file_name = list(filter(lambda s: len(s) > 0, page_address.split('/')))[-1]
 page_title = get_page_title(s)
+file_name = get_file_name(page_title)
 page_content = get_page_content(s)
 images = re.findall(r'<img.*?>', page_content)
 images_names = list(map(lambda s: re.findall(r'src=".*?"', s)[0][len('src="') : -1], images))
 images = []
-for num, i in enumerate(images_names):
-	if i[:5] != 'http:':
-		images_names[num] = 'http:' + i
+images_names = list(map(lambda s: s if s[:5] == 'http:' else 'http:' + s, images_names))
 for i in images_names:
 	images += [str(base64.b64encode(urlopen(i).read()))[2 : -1]]
 page_content = re.sub(r'<img src=".*?>', '<image l:href="#img' + chr(0) + '.jpg"/>', page_content)
